49fe879f9b3007998c5399531249b101fabb2887
alibenam <90922923+alibenam@users.noreply.github.com>
2026-04-24 16:46:01 +0200

    Initial commit

---
+++ tests/ignored/test_ignored.py
@@ -0,0 +1,11 @@
+#!python3
+# -*- coding: utf-8 -*-
+"""
+This test is ignored when launching pytest command because located in tests/ignored (as configured in tox.ini).
+"""
+
+from mobts import hello_world
+
+
+def test_hello_world():
+    assert hello_world() is None
