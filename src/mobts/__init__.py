eca4cb14c35e6f0e723fc5d0381a08c30ad6dd45
alibenam <90922923+alibenam@users.noreply.github.com>
2026-05-12 09:39:23 +0200

    version added to init

--- src/mobts/__init__.py
+++ src/mobts/__init__.py
@@ -2,3 +2,7 @@ from .main import hello_world
 
 from .preprocessing import run_preprocess_stage_1, apply_threshold, preprocess
 from .imputation import impute
+
+from importlib.metadata import version
+
+__version__ = version("mobts")
